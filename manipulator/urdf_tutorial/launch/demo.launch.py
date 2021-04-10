2ca787de7132dbed56540e3c630254335af8aea8
ania <ania@example.com>
2021-04-10 20:44:43 +0200

    new changes

--- manipulator/urdf_tutorial/launch/demo.launch.py
+++ manipulator/urdf_tutorial/launch/demo.launch.py
@@ -2,14 +2,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    urdf_file_name = 'r2d2.urdf.xml'
+    urdf_file_name = 'r2d2.urdf.xacro.xml'
     urdf = os.path.join(
         get_package_share_directory('urdf_tutorial'),
         urdf_file_name)
@@ -26,11 +26,11 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
-            arguments=[urdf]),
-        Node(
-            package='urdf_tutorial',
-            executable='state_publisher',
-            name='state_publisher',
-            output='screen'),
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': Command(['xacro', ' ', urdf])
+            }]),
+#        Node
+#            package='manip',
+#            executable='state_publisher',
+#            name='state_publisher',
+#            output='screen'),
     ])
