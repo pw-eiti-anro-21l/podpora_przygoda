bdb8bd2978e4ca48964d4bc7c2a6d969d6f4e9cd
ppodpor <80166781+ppodpor@users.noreply.github.com>
2021-04-11 22:10:06 +0200

    adding new line in converter

--- manipulator/urdf_tutorial/converter.py
+++ manipulator/urdf_tutorial/converter.py
@@ -5,10 +5,14 @@ import math
 import mathutils
 import numpy
 
+theta4 = 0
+d = 0.2
+axis_x = (1, 0, 0)
+axis_z = (0, 0, 1)
 with open('DH.json','r') as file:
 	params = json.loads(file.read())
 
-with open('urdf.yaml','w') as file:
+with open('urdf2.yaml','w') as file:
 	for key in params.keys():
 
 		a, d, alpha, theta = params[key]
@@ -19,6 +23,10 @@ with open('urdf.yaml','w') as file:
 
 		axis_x = (1, 0, 0)
 		axis_z = (0, 0, 1)
+		
+		if key==i3:
+			theta4 = theta
+			theta = 0
 
 		rot_x = mathutils.Matrix.Rotation(alpha,4,'X')
 		trans_x = mathutils.Matrix.Translation((a,0,0))
@@ -30,8 +38,25 @@ with open('urdf.yaml','w') as file:
 		xyz = dh.to_translation()
 
 		file.write(key + ":\n")
-		file.write("xyz=\"{} {} {}\"\n".format(*xyz)) #komentarz na przyszlosc, ogarnac nazwy
-		file.write("rpy=\"{} {} {}\"\n".format(*rpy))
-		file.write(" link_xyz: {} 0 0 \n".format(xyz[0]/2))
+		file.write("joint_xyz=\"{} {} {}\"\n".format(*xyz))
+		file.write("joint_rpy=\"{} {} {}\"\n".format(*rpy))
+		file.write(" link_xyz: 0 0 0 \n")
 		file.write(" link_rpy: 0 0 0 \n")
-		file.write(" link_length: {} \n\n".format(a))
+		file.write(" link_length: {} \n".format(d))
+
+	a, d, alpha = 0.0
+	rot_x = mathutils.Matrix.Rotation(alpha,4,'X')
+	trans_x = mathutils.Matrix.Translation((a,0,0))
+	rot_z = mathutils.Matrix.Rotation(theta4,4,'Z')
+	trans_z = mathutils.Matrix.Translation((0,0,d))
+
+	dh = trans_x @ rot_x @ rot_z @ trans_z
+	rpy = dh.to_euler()
+	xyz = dh.to_translation()
+
+	file.write("i4" + ":\n")
+	file.write("joint_xyz=\"{} {} {}\"\n".format(*xyz))
+	file.write("joint_rpy=\"{} {} {}\"\n".format(*rpy))
+	file.write(" link_xyz: 0 0 0 \n")
+	file.write(" link_rpy: 0 0 0 \n")
+	file.write(" link_length: {} \n".format(d))
