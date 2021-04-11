4093fc1daf9ba3e84e25a72e8d9ace3903516987
ppodpor <80166781+ppodpor@users.noreply.github.com>
2021-04-11 22:19:30 +0200

    Update converter.py

--- manipulator/urdf_tutorial/converter.py
+++ manipulator/urdf_tutorial/converter.py
@@ -7,6 +7,8 @@ import numpy
 
 theta4 = 0
 d = 0.2
+rad_s = 0.05
+rad_m = 0.1
 axis_x = (1, 0, 0)
 axis_z = (0, 0, 1)
 with open('DH.json','r') as file:
@@ -36,27 +38,38 @@ with open('urdf2.yaml','w') as file:
 		dh = trans_x @ rot_x @ rot_z @ trans_z
 		rpy = dh.to_euler()
 		xyz = dh.to_translation()
+		
+		if theta ==0:
+			rad = rad_s
+		else:
+			rad = rad_m
 
 		file.write(key + ":\n")
-		file.write("joint_xyz=\"{} {} {}\"\n".format(*xyz))
-		file.write("joint_rpy=\"{} {} {}\"\n".format(*rpy))
+		file.write(" joint_xyz=\"{} {} {}\"\n".format(*xyz))
+		file.write(" joint_rpy=\"{} {} {}\"\n".format(*rpy))
 		file.write(" link_xyz: 0 0 0 \n")
 		file.write(" link_rpy: 0 0 0 \n")
 		file.write(" link_length: {} \n".format(d))
+		file.write(" radius: {} \n".format(rad))
 
 	a, d, alpha = 0.0
+	theta = theta4
 	rot_x = mathutils.Matrix.Rotation(alpha,4,'X')
 	trans_x = mathutils.Matrix.Translation((a,0,0))
-	rot_z = mathutils.Matrix.Rotation(theta4,4,'Z')
+	rot_z = mathutils.Matrix.Rotation(theta,4,'Z')
 	trans_z = mathutils.Matrix.Translation((0,0,d))
 
 	dh = trans_x @ rot_x @ rot_z @ trans_z
 	rpy = dh.to_euler()
 	xyz = dh.to_translation()
-
+	if theta ==0:
+		rad = rad_s
+	else:
+		rad = rad_m
 	file.write("i4" + ":\n")
 	file.write("joint_xyz=\"{} {} {}\"\n".format(*xyz))
 	file.write("joint_rpy=\"{} {} {}\"\n".format(*rpy))
 	file.write(" link_xyz: 0 0 0 \n")
 	file.write(" link_rpy: 0 0 0 \n")
 	file.write(" link_length: {} \n".format(d))
+	file.write(" radius: {} \n".format(rad))
