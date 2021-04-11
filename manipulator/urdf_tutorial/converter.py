f99c14825226d9a672e4b5b9e9b34493f2e13bc7
ania <ania@example.com>
2021-04-12 01:15:19 +0200

    final version, working converter

--- manipulator/urdf_tutorial/converter.py
+++ manipulator/urdf_tutorial/converter.py
@@ -6,15 +6,14 @@ import mathutils
 import numpy
 
 theta4 = 0
-d = 0.2
-rad_s = 0.05
-rad_m = 0.1
+len = 0.2
+angle = 1.56
 axis_x = (1, 0, 0)
 axis_z = (0, 0, 1)
 with open('DH.json','r') as file:
 	params = json.loads(file.read())
 
-with open('urdf2.yaml','w') as file:
+with open('urdf/urdf.yaml','w') as file:
 	for key in params.keys():
 
 		a, d, alpha, theta = params[key]
@@ -25,51 +24,34 @@ with open('urdf2.yaml','w') as file:
 
 		axis_x = (1, 0, 0)
 		axis_z = (0, 0, 1)
-		
-		if key==i3:
-			theta4 = theta
-			theta = 0
+		rad = 0.1
 
-		rot_x = mathutils.Matrix.Rotation(alpha,4,'X')
+		rot_x = mathutils.Matrix.Rotation(alpha,4, 'X')
 		trans_x = mathutils.Matrix.Translation((a,0,0))
-		rot_z = mathutils.Matrix.Rotation(theta,4,'Z')
+		rot_z = mathutils.Matrix.Rotation(theta,4, 'Z')
 		trans_z = mathutils.Matrix.Translation((0,0,d))
 
 		dh = trans_x @ rot_x @ rot_z @ trans_z
 		rpy = dh.to_euler()
 		xyz = dh.to_translation()
 		
-		if theta ==0:
-			rad = rad_s
-		else:
-			rad = rad_m
+		if key=="i1" or key=="i3":
+			rad=rad/2
+			
 
 		file.write(key + ":\n")
-		file.write(" joint_xyz=\"{} {} {}\"\n".format(*xyz))
-		file.write(" joint_rpy=\"{} {} {}\"\n".format(*rpy))
+		if key=="i1" or key=="i2" or key=="i4":
+			xyz[2]=xyz[2]+len
+		if key=="i3":
+			rpy[1]=rpy[1]+angle
+		if key=="i4":
+			rpy[1]=rpy[1]-angle
+
+		file.write(" joint_xyz: {} {} {}\n".format(*xyz))
+		file.write(" joint_rpy: {} {} {}\n".format(*rpy))
 		file.write(" link_xyz: 0 0 0 \n")
 		file.write(" link_rpy: 0 0 0 \n")
-		file.write(" link_length: {} \n".format(d))
+		file.write(" link_length: {} \n".format(len))
 		file.write(" radius: {} \n".format(rad))
 
-	a, d, alpha = 0.0
-	theta = theta4
-	rot_x = mathutils.Matrix.Rotation(alpha,4,'X')
-	trans_x = mathutils.Matrix.Translation((a,0,0))
-	rot_z = mathutils.Matrix.Rotation(theta,4,'Z')
-	trans_z = mathutils.Matrix.Translation((0,0,d))
 
-	dh = trans_x @ rot_x @ rot_z @ trans_z
-	rpy = dh.to_euler()
-	xyz = dh.to_translation()
-	if theta ==0:
-		rad = rad_s
-	else:
-		rad = rad_m
-	file.write("i4" + ":\n")
-	file.write("joint_xyz=\"{} {} {}\"\n".format(*xyz))
-	file.write("joint_rpy=\"{} {} {}\"\n".format(*rpy))
-	file.write(" link_xyz: 0 0 0 \n")
-	file.write(" link_rpy: 0 0 0 \n")
-	file.write(" link_length: {} \n".format(d))
-	file.write(" radius: {} \n".format(rad))
