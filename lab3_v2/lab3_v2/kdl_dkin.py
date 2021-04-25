45e8c39e120debbfb132ad8d16ee9017dd6bbcee
ppodpor <80166781+ppodpor@users.noreply.github.com>
2021-04-25 16:00:19 +0200

    Update kdl_dkin.py

--- lab3_v2/lab3_v2/kdl_dkin.py
+++ lab3_v2/lab3_v2/kdl_dkin.py
@@ -26,3 +26,97 @@ class KDL_DKIN(Node):
 			self.listener_callback,
 			10)
 		self.subscription
+		
+	def listener_callback(self,msg):
+
+		with open('urdf.yaml', 'r') as file: #na razie tutaj bez sciezki
+			params = yaml.load(file, Loader=yaml.FullLoader)
+
+		params_list = []
+		params_float = []
+		params_list.add(params['i1']['joint_rpy'])
+		params_list.add(params['i1']['joint_xyz'])
+		params_list.add(params['i2']['joint_rpy'])
+		params_list.add(params['i1']['joint_xyz'])
+		params_list.add(params['i3']['joint_rpy'])
+		params_list.add(params['i3']['joint_xyz'])
+
+		for param in params_list:
+			params_split = param.split()
+			params_float.append([float(param_split) for param_split in params_split ])
+
+		chain = self.create_chain()
+
+		joint_positions=JntArray(3)
+
+		for i in range(0,3):
+			joint_positions[i]= msg.position[i]
+
+		fk=ChainFkSolverPos_recursive(chain)
+		frame = Frame()
+		fk.JntToCart(joint_positions, frame)
+
+		t_offset = Vector(0,0,0)
+		xyz = frame.p + t_offset
+		qua = frame.M.GetQuaternion()
+
+		qos_profile = QoSProfile(depth=10)
+		pose_publisher = self.create_publisher(PoseStamped, '/pose_stamped', qos_profile)
+
+		pose_stamped = PoseStamped()
+		now = self.get_clock().now()
+		pose_stamped.header.stamp = ROSClock().now().to_msg()
+		pose_stamped.header.frame_id = "base_link"
+
+		pose_stamped.pose.position.x = xyz[0]
+		pose_stamped.pose.position.y = xyz[1]
+		pose_stamped.pose.position.z = xyz[2]
+		pose_stamped.pose.orientation = Quaternion(w=qua[0], x=qua[1], y=qua[2], z=qua[3])
+
+		pose_publisher.publish(pose_stamped)
+
+
+
+	def create_chain(self):
+		chain = Chain()
+
+		link_base_1 = Joint(Joint.TransZ)
+		frame_base_1 = Frame(Rotation.RPY(
+			params_float[0][0],
+			params_float[0][1],
+			params_float[0][2]))
+		segment_base_1 = Segment(link_base_1, frame_base_1)
+
+		link_1_2 = Joint(Joint.TransZ)
+		frame_1_2 = Frame(Rotation.RPY(
+			params_float[1][0],
+			params_float[1][1],
+			params_float[1][2]))
+		segment_1_2 = Segment(link_1_2, frame_1_2)
+
+		link_2_3 = Joint(Joint.TransZ)
+		frame_2_3 = Frame(Rotation.RPY(
+			params_float[2][0],
+			params_float[2][1],
+			params_float[2][2]))
+		segment_2_3 = Segment(link_2_3, frame_2_3)
+
+		chain.addSegment(segment_base_1)
+		chain.addSegment(segment_1_2)
+		chain.addSegment(segment_2_3)
+
+		return chain
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    kdl = KDL_DKIN()
+    rclpy.spin(kdl)
+    kdl.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
